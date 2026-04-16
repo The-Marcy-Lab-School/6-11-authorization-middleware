a6642b9189f8b25134602a9a370c5674e00586c2
benspector-mls <ben@marcylabschool.org>
2026-04-16 10:11:32 -0400

    simplifying

--- server/middleware/checkAuthentication.js
+++ server/middleware/checkAuthentication.js
@@ -1,9 +1,7 @@
 // middleware/checkAuthentication.js
 const checkAuthentication = (req, res, next) => {
-  const { userId } = req.session;
-
   // No session: user is not logged in, cut off the request before continuing to the controller
-  if (!userId) {
+  if (!req.session.userId) {
     return res.status(401).send({ message: 'You must be logged in to do that.' });
   }
 
